628ab437928e03e2da583a3444928fc6111a2bd4
priyanshu123yd <114083840+priyanshu123yd@users.noreply.github.com>
2022-09-21 19:19:52 +0530

    Create basicdatatypes.cpp

---
+++ assignment 1/basicdatatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double f;
+    cin>>a>>b>>c>>d>>f;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<setprecision(10)<<d<<endl;
+    cout<<setprecision(10)<<f<<endl;
+    
+    
+    return 0;
+}
