0f9a18c5ad8679ac25988053fa0461ab6d224161
priyanshu123yd <114083840+priyanshu123yd@users.noreply.github.com>
2022-10-13 20:57:48 +0530

    Create kangaroo.cpp

---
+++ Assignment 4/kangaroo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main(){
+    int x1;
+    int v1;
+    int x2;
+    int v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if ((v1 <= v2) || ((x2 - x1) % (v2 - v1))) {
+        puts("NO");
+    } else {
+        puts("YES");
+    }
+    return 0;
+}
